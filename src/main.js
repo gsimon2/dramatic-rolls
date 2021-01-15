1db41337d227a6b147bbe913acd30dcaafa7e757
Glen Simon <glen.a.simon@gmail.com>
2021-01-15 10:35:32 -0500

    Add confetti mod support

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import soundEffectController from './soundEffectController.js';
 
 const mod = 'dramatic-rolls';
 let diceSoNiceActive = false;
+let foundryVttConfettiActive = false;
 let pendingDiceSoNiceRolls = new Map();
 
 Hooks.on('init', () => {
     game.settings.register(mod, 'add-sound', {
-        name: 'Add sound effect to natural twenties',
+        name: 'Add sound effect to natural twenties and natural ones',
         // hint: '',
         scope: 'world',
         config: true,
@@ -19,35 +20,72 @@ Hooks.on('diceSoNiceInit', () => {
     diceSoNiceActive = true;
 });
 
+Hooks.on('confettiReady', () => {
+    foundryVttConfettiActive = true;
+
+    game.settings.register(mod, 'add-confetti', {
+        name: 'Add confetti to natural twenties',
+        // hint: '',
+        scope: 'world',
+        config: true,
+        default: true,
+        type: Boolean,
+    });
+});
+
 Hooks.on('createChatMessage', (msg) => {
     let roll = msg._roll;
-    if (roll) {
+
+    if (roll && msg.user.data._id == game.userId) {
         roll = game.settings.get(mod, 'add-sound') ? attachSoundEffectIfNeeded(roll) : roll;
 
         if (diceSoNiceActive) {
             pendingDiceSoNiceRolls.set(msg.id, roll);
         } else {
-            playSound(roll);
+            handleEffects(roll);
         }
     }
 });
 
 Hooks.on('diceSoNiceRollComplete', (msgId) => {
     const roll = pendingDiceSoNiceRolls.get(msgId);
-    playSound(roll);
+    handleEffects(roll);
     pendingDiceSoNiceRolls.delete(msgId);
 });
 
-const attachSoundEffectIfNeeded = (roll) => {
+const isCrit = (roll) => {
     if (roll._formula.includes('1d20')) {
         if (roll.results[0] == 20) {
-            mergeObject(roll, {soundEffect: soundEffectController.getCritSoundEffect()});
-        } else if (roll.results[0] == 1) {
-            mergeObject(roll, {soundEffect: soundEffectController.getFumbleSoundEffect()});
+            return true;
         }
+    }
+    return false;
+};
+
+const isFumble = (roll) => {
+    if (roll._formula.includes('1d20')) {
+        if (roll.results[0] == 1) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const attachSoundEffectIfNeeded = (roll) => {
+    if (isCrit(roll)) {
+        mergeObject(roll, {soundEffect: soundEffectController.getCritSoundEffect()});
+    }
+
+    if (isFumble(roll)) {
+        mergeObject(roll, {soundEffect: soundEffectController.getFumbleSoundEffect()});
     }
 
     return roll;
+};
+
+const handleEffects = (roll) => {
+    handleConfetti(roll);
+    playSound(roll);
 };
 
 const playSound = (roll) => {
@@ -62,5 +100,18 @@ const playSound = (roll) => {
         }, true);
     }
 };
+
+const handleConfetti = (roll) => {
+    try {
+        if (game.settings.get(mod, 'add-confetti') && isCrit(roll)) {
+            const strength = window.confetti.confettiStrength.high;
+            const shootConfettiProps = window.confetti.getShootConfettiProps(strength);
+            mergeObject(shootConfettiProps, {'sound': null});
+            window.confetti.shootConfetti(shootConfettiProps);
+        }
+    } catch {
+        // Oh well, means the confetti mod isn't installed
+    }
+}
 
 
